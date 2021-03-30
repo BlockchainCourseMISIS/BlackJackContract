7eafd011cf19f8e68b685271577651121e4febfa
mike56k <isachenko.mikhail.k@gmail.com>
2021-03-30 19:25:25 +0300

    hit+stand

--- contracts/BlackJack.sol
+++ contracts/BlackJack.sol
@@ -5,7 +5,6 @@ contract BlackJack {
     struct Player {
         address playerAddress; //имя игрока
         uint32 chipsAmmount; //количество фишек
-        address delegate; // дилер, с которым он играет
         bool hasCards;
         bool authorized;
         uint32 sumPlayer;
@@ -19,9 +18,21 @@ contract BlackJack {
     Card[] public deck; //колода карт
     Card[] public dealerCards; //карты дилера
     address public dealer; //адрес дилера
+    bool public standP; // сделал ли стэнд игрок
+    bool public standD; // сделал ли стэнд дилер
+    bool public push;
+    address public winner;
+
+    uint256 public value;
+
     uint32 public ammountOfCards;
-    uint32 public dealerSum;
+    uint256 public sumDealer; // сумма очков
     mapping(address => Player) public players; // все адреса являются игроками
+
+    // function init() public payable {
+    //     dealer = payable(msg.sender);
+    //     value = msg.value;
+    // }
 
     function authorize(address player, uint32 chips) public dealerOnly {
         //авторизуем
@@ -29,12 +40,12 @@ contract BlackJack {
         players[player].authorized = true;
         players[player].hasCards = false;
         players[player].sumPlayer = 0;
-        players[player].delegate = msg.sender;
         players[player].chipsAmmount = chips;
     }
 
-    function proccessCard(address player, uint256 card) private dealerOnly {
+    function proccessCard(address player, uint256 card) private {
         players[player].cards.push(deck[card]);
+        players[player].sumPlayer += deck[card].rate;
         deck[card] = deck[ammountOfCards - 1];
         delete deck[ammountOfCards - 1];
         ammountOfCards--;
@@ -49,7 +60,7 @@ contract BlackJack {
             //если дилер выдает сам себе карту
             uint256 card = rand();
             dealerCards.push(deck[card]);
-            dealerSum += deck[card].rate;
+            sumDealer += deck[card].rate;
 
             deck[card] = deck[ammountOfCards - 1];
             delete deck[ammountOfCards - 1];
@@ -65,21 +76,64 @@ contract BlackJack {
         players[player].hasCards = true;
     }
 
-    function hit() public {}
+    function hit() public {
+        //взять еще одну карту
+        if (msg.sender == dealer) {
+            require(
+                sumDealer < 17,
+                "Dealer can't hit if he has more than 16 points"
+            );
+            uint256 cardDealer = rand();
+            dealerCards.push(deck[cardDealer]);
+            sumDealer += deck[cardDealer].rate;
+            deck[cardDealer] = deck[ammountOfCards - 1];
+            delete deck[ammountOfCards - 1];
+            ammountOfCards--;
+        } else {
+            require(
+                players[msg.sender].sumPlayer < 22,
+                "Player can't hit if he has more than 21 points"
+            );
+            uint256 cardPlayer = rand();
+            proccessCard(msg.sender, cardPlayer);
+        }
+    }
 
-    function push() public {}
+    function stand() public {
+        // завершить набор карт
+
+        if (msg.sender == dealer) {
+            standD = true;
+        } else {
+            standP = true;
+        }
+    }
 
     function checkScore(address player) public returns (uint32) {
         require(
             players[player].hasCards,
             "Player doesn't have cards" // у игрока нет карт
         );
-        lastValue = 0;
-        for (uint8 i = 0; i < players[player].cards.length; i++) {
-            lastValue += players[player].cards[i].rate;
-        }
-        players[player].sumPlayer = lastValue;
+        // for (uint8 i = 0; i < players[player].cards.length; i++) {
+        //     lastValue += players[player].cards[i].rate;
+        // }
+        lastValue = players[player].sumPlayer;
         return lastValue;
+    }
+
+    function checkWinner() public {
+        require(standP == true && standD == true, "Not all made 'stand");
+        if (
+            (players[msg.sender].sumPlayer > sumDealer) &&
+            (players[msg.sender].sumPlayer <= 21)
+        ) {
+            winner = msg.sender;
+        } else if (players[msg.sender].sumPlayer == sumDealer) {
+            push = true;
+            winner = address(0);
+        } else {
+            winner = dealer;
+        }
     }
 
     constructor() public {
